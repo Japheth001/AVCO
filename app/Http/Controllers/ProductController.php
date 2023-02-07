ba6350f6ff4db5a9c2b49484ca712a091b69c03d
emmanuel mwei <oscarkipkoech@gmail.com>
2023-02-07 21:18:10 +0300

    set product quantity

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,15 +6,17 @@ use Carbon\Carbon;
 use App\Models\Brand;
 use App\Models\Products;
 use App\Models\NewCatModel;
+use App\Models\ProductQuantityUpdate;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\ProductRequest;
 use Intervention\Image\Facades\Image;
-use Illuminate\Support\Str;
 
 class ProductController extends Controller
 {
-    public function AllProduct()
-    {
+    public $productId;
+    public function AllProduct(){
         $brands = Brand::all();
         $categories = NewCatModel::all();
         $products = Products::latest()->paginate(4);
@@ -29,40 +31,39 @@ class ProductController extends Controller
         return view('admin.products.add', compact('products', 'categories', 'brands'));
     }
 
-    public function AddProduct(Request $request)
-    {
+    public function AddProduct(Request $request){
 
         // $categories = NewCatModel::findOrFail('category_id');
         // // $product=$categories->products()->create([
         // $product = $categories->products()->Products::insert([
             $product_image = $request->file('image');
 
-            $name_gen = md5(rand(1000, 10000)) . '.' . $product_image->getClientOriginalExtension();
-            // Image::make($multi_img)->resize(300, 300)->save('image/multi/'.$name_gen);
-            // $img = Image::make('foo.jpg')->resize(300, 200);
-            Image::make($product_image)->resize(300, 200)->save('storage/images/product/' . $name_gen);
-            $last_img = 'images/product/' . $name_gen;
+                $name_gen = md5(rand(1000, 10000)) . '.' . $product_image->getClientOriginalExtension();
+                // Image::make($multi_img)->resize(300, 300)->save('image/multi/'.$name_gen);
+                // $img = Image::make('foo.jpg')->resize(300, 200);
+                Image::make($product_image)->resize(300, 200)->save('storage/images/product/' . $name_gen);
+                $last_img = 'images/product/' . $name_gen;
     
             
             
-            
+
             Products::insert([
 
-            'category_id' => $request->category_id,
-            'name' => $request->name,
-            'slug' => $request->slug,
-            'brand' => $request->brand,
-            'small_description' => $request->small_description,
-            'description' => $request->description,
-            'quantity' => $request->quantity,
+                'category_id' => $request->category_id,
+                'name' => $request->name,
+                'slug' => $request->slug,
+                'brand' => $request->brand,
+                'small_description' => $request->small_description,
+                'description' => $request->description,
+                'quantity' => $request->quantity,
             // 'image'=>$last_img,
-            'meta_title' => $request->meta_title,
-            'meta_keyword' => $request->meta_keyword,
-            'meta_description' => $request->meta_description,
-            'trending' => $request->trending == true ? '1' : '0',
-            'status' => $request->status == true ? '1' : '0',
+                'meta_title' => $request->meta_title,
+                'meta_keyword' => $request->meta_keyword,
+                'meta_description' => $request->meta_description,
+                'trending' => $request->trending == true ? '1' : '0',
+                'status' => $request->status == true ? '1' : '0',
             'image'=>$last_img,
-            'created_at' => Carbon::now(),
+                'created_at' => Carbon::now(),
 
         ]);
         return Redirect()->route('all.product')->with('success', 'Product Added Successfully');
@@ -112,6 +113,55 @@ class ProductController extends Controller
         $categories = NewCatModel::all();
         $products = Products::all();
         return view('admin.products.edit', compact('products','categories','brands'));
+    }
+
+    public function productQuantity(){
+        // get id from params
+        $id = request()->route('id');
+
+        // get products details where id = $id
+        $product = Products::where('id', $id)->first();
+
+        $productQuantityUpdates = ProductQuantityUpdate::where('productId', $id)
+            ->join('users', 'users.id', '=', 'product_quantity_updates.userIdUpdated')
+            ->select('product_quantity_updates.*', 'users.name as userName')
+            ->get();
+
+        return view('admin.products.quantity', compact('productQuantityUpdates', 'product'));
+    }
+
+    public function productQuantityNew($id){
+        return view ('admin.products.quantityNew')->with('id', $id);
+    }
+
+    public function saveProductQuantity(Request $request){
+        $request->validate([
+            'quantity' => 'required',
+            'description' => 'required',
+        ]);
+        $user = Auth::user();
+        $result=ProductQuantityUpdate::create([
+            'quantity' => $request->quantity,
+            'description' => $request->description,
+            'productId' => $request->productId,
+            'userIdUpdated' => $user->id,
+        ]);
+        $request->session()->flash('success', 'Product quantity updated successfully');
+        return redirect()->route('product.quantity.all', ['id' => $request->productId]);
+    }
+
+    public function productUsage(){
+        $brands = Brand::all();
+        $categories = NewCatModel::all();
+        $products = Products::latest()->paginate(4);
+        return view('admin.products.usage', compact('products', 'categories', 'brands'));
+    }
+
+    public function productUsageNew(){
+        $brands = Brand::all();
+        $categories = NewCatModel::all();
+        $products = Products::all();
+        return view('admin.products.usageNew', compact('products', 'categories', 'brands'));
     }
 
 }
---
+++ database/migrations/2023_02_05_142003_create_product_usages_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_usages', function (Blueprint $table) {
+            $table->id();
+            $table->string('quantity')->nullable();
+            $table->foreignId('vehicleId')->constrained('vehicles');
+            $table->unsignedBigInteger('productId')->nullable();
+            $table->foreign('productId')->references('id')->on('products');
+            $table->unsignedBigInteger('userIdUpdated')->nullable();
+            $table->foreign('userIdUpdated')->references('id')->on('users'); 
+
+            $table->longText('description', 10000)->nullable();
+            $table->timestamps();   
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_usages');
+    }
+};
